b3bd01203fa3a708a1373b1fb4175e71e9d069c7
zpmff <435649398@qq.com>
2017-12-14 17:27:08 +0800

    ykj  company init

---
+++ Application/Admin/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING'  =>array(
+        '__PUBLIC__' => SITE_URL.'/Application/Admin/Public',
+        '__ADMINCSS__' => SITE_URL.'/Application/Admin/Public',
+        '__ADMINIMG__' => SITE_URL.'/Application/Admin/Public',
+    ),
+);
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //前台公共文件的地址
+    'TMPL_PARSE_STRING'  =>array(
+        '__PUBLIC__' => SITE_URL.'/Public',
+        '__STYLE__' => SITE_URL.'/Public/style',
+        '__IMG__' => SITE_URL.'/Public/images',
+    ),
+);
